5b393e188a212596999f27f7a21ec725c224745f
kimtaesu <kimtaesoo188@gmail.com>
2017-09-04 20:49:06 +0900

    Initial commit

---
+++ src/test/java/TokenCollectionStateMachineTest.java
@@ -0,0 +1,11 @@
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TokenCollectionStateMachineTest {
+
+    @Test
+    public void tokenTest() {
+        Assert.assertThat("", Is.is(""));
+    }
+}
